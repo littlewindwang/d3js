582178f9adafb75592d175bee34bf8757eb87a65
littlewindwang <littlewindwang@hotmail.com>
2017-11-08 19:21:04 -0500

    updated grunt filie

--- GruntFile.js
+++ GruntFile.js
@@ -20,17 +20,27 @@ module.exports = function (grunt) {
                 src:['src/js/*.js'],
                 dest:'./build/js/total.js'
             }
+        },
+        uglify: {
+            compressjs: {
+                files: {
+                    './dist/js/total.min.js': ['./build/js/total.js']
+                }
+            }
+        },
+        jshint: {
+            all: ['./global.js']
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-concat');
-
-
-
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
     grunt.registerTask('outputcss', ['sass']);
     grunt.registerTask('concatjs', ['concat']);
+    grunt.registerTask('conpressJS',['concat','jshint','uglify'])
 
 
     grunt.registerTask('default');
